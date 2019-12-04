6458c6c7c393dfdf8abcc00ff879bd8b6c7a5975
Esteban <montesteban0@gmail.com>
2019-12-04 13:15:30 -0600

    Usar index

---
+++ ejercio1.py
@@ -0,0 +1,8 @@
+def main():
+    lista = ["Brawl stars", "Fortnite", "Call of duty black ops 3"]
+    print(lista)
+    F = lista[lista.index("Fortnite")]
+
+
+if __name__ == "__main__":
+    main()
